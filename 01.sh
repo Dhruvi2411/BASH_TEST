ada394ea6791474a9aeed03a5a3cab68c486f983
Dhruvi2411 <mangaroliyadhruvi24@gmail.com>
2026-01-28 14:24:16 +0530

    First code added

--- 01.sh
+++ 01.sh
@@ -1,18 +1,18 @@
 #!/bash/bin
 
 if (( $# == 0 )); then {
-  echo "No arguments provided."
+    echo "No arguments provided."
     exit 1
 }
 else {
-  echo "Arguments provided: $@"
-  exit 1
+    echo "Arguments provided: $@"
+    exit 1
 }
 fi
 
 
 if (( $1 == "Start" )); then
-  echo "Starting the process..."
+    echo "Starting the process..."
 
 elif (( $1 == "Stop" )); then
     echo "Stopping the process..."
