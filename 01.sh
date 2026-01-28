3b3dba20c9ad894e20aaa4160a8bc3dda78f8920
Dhruvi2411 <mangaroliyadhruvi24@gmail.com>
2026-01-28 14:19:28 +0530

    First code added

---
+++ 01.sh
@@ -0,0 +1,26 @@
+#!/bash/bin
+
+if (( $# == 0 )); then {
+  echo "No arguments provided."
+    exit 1
+}
+else {
+  echo "Arguments provided: $@"
+  exit 1
+}
+fi
+
+
+if (( $1 == "Start" )); then
+  echo "Starting the process..."
+
+elif (( $1 == "Stop" )); then
+    echo "Stopping the process..."
+
+elif (( $1 == "Status" )); then
+    echo "Application is running..."
+
+else
+    echo "Invalid option..."
+fi
+
