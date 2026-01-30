23a4fb3f18c746f7a9f8e97828e24ae5b0d00b03
Dhruvi2411 <mangaroliyadhruvi24@gmail.com>
2026-01-30 15:25:26 +0530

    Q-1 Add

--- 01/01.sh
+++ 01/01.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+# (( )) ==> comparing numbers (arithmetic evaluation)
+# [[ ]] ==> comparing strings, files, etc. (conditional expression)
+# shebang: #!/bin/bash ==> indicates the script should be run in bash shell
+# bash: read file and run commands line by line
+
+
 if (( $# == 0 )); then 
     echo "No arguments provided."
     exit 1
 fi
 
 
-if (( $1 == "Start" )); then
+if [[ $1 == "Start" ]]; then
     echo "Starting the process..."
 
-elif (( $1 == "Stop" )); then
+elif [[ $1 == "Stop" ]]; then
     echo "Stopping the process..."
 
-elif (( $1 == "Status" )); then
+elif [[ $1 == "Status" ]]; then
     echo "Application is running..."
 
 else
